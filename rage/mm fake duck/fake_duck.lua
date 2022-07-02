893fd5033bf684d70e3199ddbd5b31d77faf200a
dhdj <25525516+dhdjSYS@users.noreply.github.com>
2022-07-03 00:19:34 +0800

    improvements

--- rage/mm fake duck/fake_duck.lua
+++ rage/mm fake duck/fake_duck.lua
@@ -117,7 +117,7 @@ local button = bit.lshift(1,2)
 local function on_setup_command(cmd)
     if not fake_duck_ref:get() or game_rules.get_prop("m_bIsValveDS")==0 then return end
     local local_player = entity_list.get_local_player()
-    if local_player:get_prop("m_flDuckAmount")>0.8 then
+    if local_player:get_prop("m_flDuckAmount")>0.85 then
         button=bit.lshift(1,22)
     elseif local_player:get_prop("m_flDuckAmount")<=0.2 then
         button=bit.lshift(1,2)
@@ -130,7 +130,8 @@ callbacks.add(e_callbacks.SETUP_COMMAND,on_setup_command)
 local function on_antiaim(ctx)
     if not fake_duck_ref:get() or game_rules.get_prop("m_bIsValveDS")==0 then return end
     local local_player = entity_list.get_local_player()
-    if local_player:get_prop("m_flDuckAmount")>0.6 and local_player:get_prop("m_flDuckAmount")<0.80 then
+    local m_flDuckAmount = local_player:get_prop("m_flDuckAmount")
+    if m_flDuckAmount>0.6 and m_flDuckAmount<0.75 then
         ctx:set_fakelag(false)
     else
         ctx:set_fakelag(true)
